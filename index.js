fba0c7f3019e37097c98635e42642c18490573ba
block-doc-au <neilc@cns-gc.com.au>
2022-08-24 20:28:28 +1000

    NC Completed JS Array Find lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function superbowlWin (theArray){
+ let myResult = theArray.find( year => {return year.result === 'W'});
+ console.log(myResult);
+ if (myResult == undefined) {return undefined;}
+     let theYear = myResult.year;
+    console.log(theYear);
+    return theYear;
+     }
+ 
